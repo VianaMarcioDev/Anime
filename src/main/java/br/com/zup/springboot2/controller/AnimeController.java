7353e7aa240d477db3944142c062136867714cae
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2021-11-14 18:19:23 -0300

    Usar Lombok e LocalDateTime formatado

--- src/main/java/br/com/zup/springboot2/controller/AnimeController.java
+++ src/main/java/br/com/zup/springboot2/controller/AnimeController.java
@@ -2,15 +2,18 @@ package br.com.zup.springboot2.controller;
 
 import br.com.zup.springboot2.domain.Anime;
 import br.com.zup.springboot2.util.DateUtil;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("anime")
+@Log4j2
 public class AnimeController {
 
     @Autowired
@@ -18,6 +21,7 @@ public class AnimeController {
 
     @GetMapping(path = "list")
     public List<Anime>list(){
+        log.info(dateUtil.formatDateTimeToDatabaseStyle(LocalDateTime.now()));
         return List.of(new Anime("DBZ"), new Anime("Berserk"));
     }
 }
