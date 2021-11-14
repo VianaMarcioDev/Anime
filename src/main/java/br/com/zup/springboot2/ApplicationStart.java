6ffe084f6ca2734f7a4be29867b64bbdab706a5c
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2021-11-14 18:24:35 -0300

    Usar annotation @SpringBootApplication

--- src/main/java/br/com/zup/springboot2/ApplicationStart.java
+++ src/main/java/br/com/zup/springboot2/ApplicationStart.java
@@ -2,10 +2,10 @@ package br.com.zup.springboot2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class ApplicationStart {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStart.class, args);
